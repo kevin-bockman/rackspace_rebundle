907190fa04cd344454df3dcb0372dabe1e2996f6
kbockmanrs <kevin@rightscale.com>
2011-09-10 08:04:50 +0000

    [WIP] Update Ubuntu, New tools, Major refactor

---
+++ default/clean.sh
@@ -0,0 +1,81 @@
+#!/bin/bash -x
+source functions.sh
+
+#
+# Clean image before bundling
+#
+
+clean()
+{
+  echo "Cleaning logfile: $1"
+  [ -f "$1" ] && cat /dev/null > $1
+  rm -f $1.[12345]
+}
+
+#
+# /etc/rightscale.d files
+#
+cp /etc/rightscale.d/rightscale-release /tmp/rightscale-release
+rm -rf /etc/rightscale.d
+mkdir /etc/rightscale.d
+echo -n "rackspace" > /etc/rightscale.d/cloud
+cp /tmp/rightscale-release /etc/rightscale.d/rightscale-release 
+
+#
+# State information
+#
+rm -f /var/spool/cloud/*
+service postfix stop
+find /var/spool -type f -exec ~/truncate.sh {} \;
+rm -rf /tmp/* /tmp/.*
+
+#
+# Package manager cleanup
+#
+case $os in
+"centos")
+  yum -y clean all
+  ;;
+"ubuntu")
+  apt-get clean
+  mkdir /tmp/agent-smith
+
+  sed -i s/root::/root:*:/ /etc/shadow
+  ;; 
+esac
+
+#
+# Log files
+#
+find /var/log -type f -exec ~/truncate.sh {} \;
+
+rm -rf /var/cache/*
+rm -rf /var/mail/*
+
+find /etc -name \*~ -exec rm -- {} \;
+find /etc -name \*.backup* -exec rm -- {} \;
+
+if [ "$os" == "ubuntu" ]; then
+  # Rebuild Apt cache
+  mkdir -p /var/cache/apt/archives/partial /var/cache/debconf
+  apt-cache gencaches
+fi
+
+#
+# /etc
+#
+rm -f /etc/hosts.backup.*
+
+cp /root/files/sshd_config /etc/ssh
+rm -rf /etc/ssh/ssh_host_*
+
+#
+# /root
+#
+rm -rf /root/.ssh
+rm -rf /root/.gem
+rm -f /root/*.tar
+rm -rf /root/files
+rm -f /root/*
+rm -f /root/.bash_history /root/.vim* /root/.lesshst
+rm -rf /root/.cache
---
+++ default/functions.sh
@@ -0,0 +1,98 @@
+os=$(lsb_release -is|tr '[:upper:]' '[:lower:]'|tr -d '\n')
+rightlink_ver=$1
+rightlink_os=$os
+rightlink_os_ver=$(lsb_release -rs)
+
+download() {
+  case $rightlink_os in
+  "centos")
+    rightlink_pkg="rpm"
+    rightlink_arch="x86_64"
+    ;;
+  "ubuntu")
+    rightlink_pkg="deb"
+    rightlink_arch="amd64"
+    ;;
+  *)
+    echo "FATAL: Unknown OS"
+    exit 1
+    ;;
+  esac
+  
+  rightlink_file="rightscale_${rightlink_ver}-${rightlink_os}_${rightlink_os_ver}-${rightlink_arch}.${rightlink_pkg}" 
+  buckets=( rightscale_rightlink rightscale_rightlink_dev )
+  locations=( $rightlink_ver/$rightlink_os $rightlink_ver / )
+
+  [ -f /root/.rightscale/$rightlink_file ] && return 0
+ 
+  set +e 
+  for bucket in ${buckets[@]}
+  do
+    for location in ${locations[@]}
+    do
+      code=$(curl -o /root/.rightscale/${rightlink_file} --connect-timeout 10 --fail --silent --write-out %{http_code} http://s3.amazonaws.com/$bucket/$location/${rightlink_file})
+      return=$?
+      echo "BUCKET: $bucket LOCATION: $location RETURN: $return CODE: $code"
+      [[ "$return" -eq "0" && "$code" -eq "200" ]] && break 2
+    done
+  done
+
+  if [ "$?" == "0" ]; then
+    set -e
+    # Install RightLink seed script
+    install /tmp/sandbox_builds/seed_scripts/rightimage /etc/init.d/rightimage --mode=0755
+
+    case $rightlink_os in
+    "centos")
+      agent="nova-agent"
+      chkconfig --add rightimage
+      ;;
+    "ubuntu")
+      agent="agent-smith"
+      update-rc.d rightimage defaults
+      ;;
+    esac
+
+    sed -i "s/amazon$/${agent}/g" /etc/init.d/rightimage
+    post
+    return 0
+  fi
+
+  return 1
+}
+
+build() {
+  case $rightlink_os in
+  "centos")
+    pkg_mgr="yum" 
+    pkg_type="rpm"
+    rake="-v 0.8.7"
+    ;;
+  "ubuntu")
+    pkg_mgr="apt-get"
+    pkg_type="deb"
+    rake=""
+    ;;
+  esac
+   
+  $pkg_mgr -y install rubygems 
+  gem install --remote rake $rake
+  
+  if [ -d /opt/rightscale/sandbox ]; then mv /opt/rightscale/sandbox /opt/rightscale/sandbox.OLD; fi
+  cd /tmp/sandbox_builds
+  export ARCH="x86_64"
+  export RS_VERSION=$rightlink_ver
+  rake submodules:sandbox:create
+  rake right_link:$pkg_type:build
+  
+  rm -rf /root/.rightscale
+  mkdir /root/.rightscale
+  cp dist/*.$pkg_type /root/.rightscale
+  post
+}
+
+post() {
+  echo $rightlink_ver > /etc/rightscale.d/rightscale-release
+  chmod 0770 /root/.rightscale
+  chmod 0440 /root/.rightscale/*
+}
